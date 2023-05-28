739252206cb7f2008628aa02a2dd4eded8d8ff4a
jky <jkypch@gmail.com>
2023-05-28 13:50:17 +0900

    commit

--- database/favoredBean.py
+++ database/favoredBean.py
@@ -1,37 +1,18 @@
 from fastapi import HTTPException
-from motor.motor_asyncio import AsyncIOMotorClient
-from dotenv import load_dotenv, find_dotenv
 from datetime import datetime
-from database.models import *
-import os
-from typing import List
-
-# 환경 변수를 로드합니다.
-load_dotenv(find_dotenv())
-
-# 환경 변수에서 비밀번호를 얻습니다.
-password = os.environ.get("MONGODB_PWD")
-
-# MongoDB URI를 정의합니다.
-uri = f"mongodb+srv://admin:{password}@recommend.wg2l4em.mongodb.net/?retryWrites=true&w=majority"
-
-# MongoDB 클라이언트를 생성합니다.
-client = AsyncIOMotorClient(uri)
-
-# 데이터베이스를 선택합니다.
-db = client.BeanRecommendationsDB
+from database.models import FavoredBean
+from database.database import db
 
 # 글로벌 컬렉션 변수를 정의합니다.
 # 이 변수는 MongoDB 데이터베이스 내의 FavoredBeans 컬렉션을 나타냅니다.
 FavoredBeans = db.FavoredBeans
-
 
 # 새로운 찜한 원두를 생성하는 비동기 함수입니다.
 # 이 함수는 먼저 데이터베이스에서 같은 ID를 가진 원두가 이미 있는지 확인합니다. 
 # 만약 이미 존재한다면, HTTP 오류를 발생시킵니다.
 # 그렇지 않다면, 원두 정보를 데이터베이스에 추가합니다.
 async def post_create_favoredBean(favoredBean):
-    if await FavoredBeans.find_one({"favored_bean_id": favoredBean.favoredBean_id}):
+    if await FavoredBeans.find_one({"favored_bean_id": favoredBean.favored_bean_id}):
         raise HTTPException(status_code=400, detail="FavoredBean already registered")
 
     favoredBean.created_at = datetime.now()  # 생성 날짜 설정
