9c4f6a8dec9149ad274c21b828fb8b09db176aa1
JKY99 <jkypch@gmail.com>
2023-06-05 14:10:23 +0900

    fe

--- database/UserService.py
+++ database/UserService.py
@@ -10,12 +10,12 @@ class UserService:
     async def create(cls, user: User) -> User:
         user_dict = jsonable_encoder(user)
         new_user = await cls.collection.insert_one(user_dict)
-        created_user = await cls.collection.find_one({"_id": new_user.inserted_id})
+        created_user = await cls.collection.find_one({"user_id": new_user.inserted_id})
         return User(**created_user)
 
     @classmethod
     async def read(cls, user_id: str) -> User:
-        user = await cls.collection.find_one({"_id": user_id})
+        user = await cls.collection.find_one({"user_id": user_id})
         if user:
             return User(**user)
         return None
@@ -28,15 +28,15 @@ class UserService:
     @classmethod
     async def update(cls, user_id: str, user: User) -> User:
         user_dict = jsonable_encoder(user)
-        await cls.collection.replace_one({"_id": user_id}, user_dict)
-        updated_user = await cls.collection.find_one({"_id": user_id})
+        await cls.collection.replace_one({"user_id": user_id}, user_dict)
+        updated_user = await cls.collection.find_one({"user_id": user_id})
         if updated_user:
             return User(**updated_user)
         return None
 
     @classmethod
     async def delete(cls, user_id: str) -> None:
-        delete_result = await cls.collection.delete_one({"_id": user_id})
+        delete_result = await cls.collection.delete_one({"user_id": user_id})
         if delete_result.deleted_count:
             return True
         return False
